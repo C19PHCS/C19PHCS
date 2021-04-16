e9836a8c880537f8868f87d20d26f68554cdb1c6
ariannechau <ariannechau@hotmail.com>
2021-04-16 01:56:44 -0400

    add endpoints for 13-17 add implementation update set alert

--- init.sql
+++ init.sql
@@ -10,12 +10,16 @@ create schema comp353;
 
 USE comp353;
 
-CREATE TABLE test (
-    name VARCHAR(45),
-    PRIMARY KEY (name)
+CREATE TABLE `publicHealthCenter` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `name` varchar(100) NOT NULL,
+    `address` varchar(255) NOT NULL,
+    `webAddress` varchar(255) NOT NULL,
+    `phoneNumber` varchar(15) NOT NULL,
+    `type` enum('hospital','clinic','special_installment') NOT NULL,
+    `city` varchar(45) NOT NULL,
+    `province` char(2) NOT NULL,
+    `country` varchar(45) NOT NULL,
+    `postalCode` char(6) NOT NULL,
+    PRIMARY KEY (`id`)
 );
-
-INSERT test (name)
-VALUES
-("bob")
-;
