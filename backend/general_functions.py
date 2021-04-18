cf26f933178e0d51f26f4444bb5aa8df318fee20
ariannechau <ariannechau@hotmail.com>
2021-04-18 06:55:46 -0400

    add endpoints for 13-17

--- backend/general_functions.py
+++ backend/general_functions.py
@@ -1,9 +1,19 @@
 from database import get_conn
+import datetime
 
 def get_single(table, data):
     conn = get_conn()
     cursor = conn.cursor()
-    query = ("SELECT * FROM {} WHERE id = {};").format(table, data['id'])
+    
+    temp = ""
+    for k, v in data.items():
+        if isinstance(v, str):
+            temp += "{} = '{}' and ".format(k, v)
+        else:
+            temp += "{} = {} and ".format(k, v)
+    temp = temp[:-5]
+
+    query = ("SELECT * FROM {} WHERE {};").format(table, temp)
 
     cursor.execute(query)
 
@@ -66,7 +76,16 @@ def delete(table, data):
 
     conn = get_conn()
     cursor = conn.cursor()
-    query = ("DELETE FROM {} WHERE id = {};").format(table, data['id'])
+
+    temp = ""
+    for k, v in data.items():
+        if isinstance(v, str):
+            temp += "{} = '{}' and ".format(k, v)
+        else:
+            temp += "{} = {} and ".format(k, v)
+    temp = temp[:-5]
+
+    query = ("DELETE FROM {} WHERE {};").format(table, temp)
 
     cursor.execute(query)
 
@@ -76,21 +95,50 @@ def delete(table, data):
 
     return True
 
-def edit(table, data):
+primary_keys = {
+    'person' : ['medicareNumber'],
+    'publicHealthWorker' : ['workerID'],
+    'publicHealthCenter' : ['id'],
+    'region' : ['name'],
+    'groupZone' : ['groupID'],
+    'healthRecommendation' : ['id'],
+    'healthRecommendationSecondary': ['id', 'recommendation']
+}
 
-    if not exists(table, data):
+def edit(table, data):
+    ids = {}
+    datas = {}
+
+    for key, value in data.items():
+        if key in primary_keys[table]:
+            ids[key] = value
+        else:
+            datas[key] = value
+
+    if not exists(table, ids):
         return False
 
     conn = get_conn()
     cursor = conn.cursor()
-    temp = ""
 
-    for k, v in data.items():
-        temp += "{} = '{}',".format(k, v)
+    temp = ""
+    for k, v in datas.items():
+        if isinstance(v, str):
+            temp += "{} = '{}',".format(k, v)
+        else:
+            temp += "{} = {},".format(k, v)
     temp = temp[:-1]
 
-    query = ("UPDATE {} SET {} WHERE id={}").format(table, temp, data["id"])
+    temp2 = ""
+    for k, v in ids.items():
+        if isinstance(v, str):
+            temp2 += "{} = '{}' and ".format(k, v)
+        else:
+            temp2 += "{} = {} and ".format(k, v)
+    temp2 = temp2[:-5]
 
+    query = ("UPDATE {} SET {} WHERE {};").format(table, temp, temp2)
+    
     cursor.execute(query)
 
     cursor.close()
@@ -99,7 +147,182 @@ def edit(table, data):
 
     return True
 
+# 7- set new alert for a specific region
+def set_region_alert(data):
+    create('alert', data)
+
+# 13- list of all regions
+def get_regions():
+    rows = get_all('region')
+    for region in rows:
+        cities = get_single('city', {'regionName' : region['name']})
+
+        for city in cities:
+            postal_codes = get_single('cityPostalCodeMapping', {'city' : city['name']})
+            temp = list(map(lambda x: x['postalCodeRegion'], postal_codes))
+            city['postalCodes'] = temp
+            city.pop('regionName', None)
+
+        region['cities'] = cities
+        region.pop('alertLevel', None)
+
+    return rows
+
+# 14- list of people who got the result of the test on a specific date
+def get_test_result_on_date(data):
+    conn = get_conn()
+    cursor = conn.cursor()
+    
+    query = ("""
+    SELECT P.firstName, P.lastName, P.dateOfBirth, P.phoneNumber, P.email, D.result
+FROM
+(SELECT medicareNumber, result
+FROM diagnostic
+WHERE testDate = '{}') as D, person as P
+WHERE P.medicareNumber = D.medicareNumber
+ORDER BY D.result DESC
+;""").format(data['testDate'])
+
+    cursor.execute(query)
+
+    columns = [col[0] for col in cursor.description]
+    rows = [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+    cursor.close()
+    conn.commit()
+    conn.close()
+
+    return rows
+
+# 15- list of workers in specific facility
+def get_workers_at_facility(data):
+    conn = get_conn()
+    cursor = conn.cursor()
+
+    query = ("""SELECT P.firstName, P.lastName, PHW.workerID, PHW.medicareNumber, PHW.publicHealthCenterID
+FROM publicHealthWorker as PHW, person as P
+WHERE publicHealthCenterID = {} and PHW.medicareNumber = P.medicareNumber;""").format(data['publicHealthCenterID'])
+
+    cursor.execute(query)
+
+    columns = [col[0] for col in cursor.description]
+    rows = [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+    cursor.close()
+    conn.commit()
+    conn.close()
+
+    return rows
+
+def get_all_at_risk_workers(from_date, to_date, workerid):
+    conn = get_conn()
+    cursor = conn.cursor()
+
+    query = ("""SELECT DISTINCT PHW2.workerID, P.firstName, P.lastName
+FROM workerSchedule, publicHealthWorker as PHW1, publicHealthWorker as PHW2, person as P
+WHERE 
+	workerSchedule.workerID <> {} and 
+    `date` between "{}" and "{}" and 
+    PHW1.workerID = {} and
+    PHW2.workerID = workerSchedule.workerID and
+    PHW1.publicHealthCenterID = PHW2.publicHealthCenterID and
+    PHW2.medicareNumber = P.medicareNumber;""").format(workerid, from_date, to_date, workerid)
+
+    cursor.execute(query)
+
+    columns = [col[0] for col in cursor.description]
+    rows = [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+    cursor.close()
+    conn.commit()
+    conn.close()
+    return rows
+
+# 16- list of all public health workers who tested positive on a specific date in a specific facility
+def get_workers_positive_test_at_facility(data):
+    date_formatted = datetime.datetime.strptime(data['testDate'], '%Y-%m-%d')
+    date_before = date_formatted - datetime.timedelta(days=14)
+    date_before_str = date_before.strftime("%Y-%m-%d")
+
+    conn = get_conn()
+    cursor = conn.cursor()
+
+    query = ("""SELECT P.firstName, P.lastName, PHW.medicareNumber, PHW.workerID
+FROM
+(SELECT medicareNumber, workerID
+FROM publicHealthWorker
+WHERE publicHealthCenterID = {}) as PHW, diagnostic as D, person as P
+WHERE D.result = true and 
+D.testDate = '{}'and 
+PHW.medicareNumber = D.medicareNumber and 
+PHW.medicareNumber = P.medicareNumber;""").format(data['publicHealthCenterID'], data['testDate'])
+
+    cursor.execute(query)
+
+    columns = [col[0] for col in cursor.description]
+    rows = [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+    cursor.close()
+    conn.commit()
+    conn.close()
+
+    res = []
+    for worker in rows:
+        temp = {}
+        temp['sickWorker'] = worker
+        workerid = worker['workerID']
+        temp['atRiskWorkers'] = get_all_at_risk_workers(date_before_str, data['testDate'], workerid)
+        res.append(temp)
+
+    return res
+
+# 17- report for each region
+def get_all_region_reports():
+    conn = get_conn()
+    cursor = conn.cursor()
+
+    query = ("""SELECT negative_count.regionName, positive_count.count as positiveCount, negative_count.count as negativeCount
+FROM 
+(SELECT count(P.medicareNumber) as count, D.result, C.regionName
+FROM diagnostic as D, person as P, city as C
+WHERE 
+    D.result = true and
+    D.medicareNumber = P.medicareNumber and
+    P.city = C.`name`
+GROUP BY D.result, C.regionName) as positive_count,
+(SELECT count(P.medicareNumber) as count, D.result, C.regionName
+FROM diagnostic as D, person as P, city as C
+WHERE 
+    D.result = false and
+    D.medicareNumber = P.medicareNumber and
+    P.city = C.`name`
+GROUP BY D.result, C.regionName) as negative_count
+WHERE negative_count.regionName = positive_count.regionName;""")
+
+    cursor.execute(query)
+
+    columns = [col[0] for col in cursor.description]
+    rows = [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+    cursor.close()
+    conn.commit()
+    conn.close()
+
+    for report in rows:
+        alerts = get_single('alert', {'regionName' : report['regionName']})
+        report['alertHistory'] = alerts
+        
+        for alert in alerts:
+            alert.pop('regionName', None)
+
+    return rows
+
 def perform_action(action, table, data):
+    table_list = ['person', 'publicHealthWorker', 'publicHealthCenter', 'region', 'groupZone', 'groupZoneMapping', 'diagnostic', 'healthRecommendation', 'healthRecommendationSecondary', 'message']
+
+    if table not in table_list:
+        raise Exception('invalid table')
+
     if action == 'get_single':
         return get_single(table, data)
     
@@ -113,4 +336,4 @@ def perform_action(action, table, data):
         return delete(table, data)
 
     if action == 'edit':
-        return edit(table, data) 
+        return edit(table, data)
--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from general_functions import perform_action
+from general_functions import *
 
 
 def main():
@@ -11,7 +11,35 @@ def main():
     def home():
         return "hello world"
 
-    @app.route('/<table>/<action>', methods=['POST'])
+    @app.route('/region/info', methods=['GET'])
+    def get_all_regions():
+        return jsonify(get_regions())
+
+    @app.route('/region/report', methods=['GET'])
+    def get_region_reports():
+        return jsonify(get_all_region_reports())
+
+    @app.route('/test_results_on_date', methods=['GET'])
+    def get_all_test_results_on_specific_date():
+        data = request.get_json()
+        return jsonify(get_test_result_on_date(data))
+
+    @app.route('/workers_at_facility', methods=['GET'])
+    def get_all_workers_at_facility():
+        data = request.get_json()
+        return jsonify(get_workers_at_facility(data))
+
+    @app.route('/workers_positive_test', methods=['GET'])
+    def get_workers_positive_at_facility():
+        data = request.get_json()
+        return jsonify(get_workers_positive_test_at_facility(data))
+
+    @app.route('/set_region_alert', methods=['POST'])
+    def set_alert_for_region():
+        data = request.get_json()
+        return jsonify(set_region_alert(data))
+
+    @app.route('/general/<table>/<action>', methods=['POST'])
     def actions(table, action):
         data = request.get_json()
         return jsonify(perform_action(action, table, data))
